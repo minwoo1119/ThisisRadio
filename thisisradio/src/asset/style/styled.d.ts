13098b95993cc6ff0153dc2f6e1dc5507d7a065b
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-03-01 15:18:30 +0900

    [feat] #1 - expo 프로젝트 생성 및 theme.ts 파일 생성

---
+++ thisisradio/src/asset/style/styled.d.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components/native";
+
+export interface DefaultTheme {
+	mainBackground: string;
+	mainfont: string;
+	mainButton: string;
+	subColor: string;
+}
---
+++ thisisradio/src/asset/style/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components/native";
+
+const theme: DefaultTheme = {
+	mainBackground: "#121721",
+	mainfont: "#FFFFFF",
+	mainButton: "#1A5CE5",
+	subColor: "#94A6C7",
+};
